9fd04eb2447bb473f0cbdd71deb97012a9c40644
Daniel <daniel.bnh13gmail.com>
2022-03-20 20:05:34 -0300

    a

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Tela2 from "./Tela2";
+import Corpo from "./Corpo";
+export default  function App() {
+    return (
+      <div>
+          <Corpo />
+          <Tela2 />
+      </div>
+     
+    );
+    
+  }
